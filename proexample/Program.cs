b97edf628ff0b2b60d9aebcf33bf3963284cf464
DrJuliaKochetkova <132363211+DrJuliaKochetkova@users.noreply.github.com>
2023-05-24 15:42:36 +0300

    Update Program.cs

--- proexample/Program.cs
+++ proexample/Program.cs
@@ -1,12 +1,1 @@
-﻿Console.WriteLine("Введите два числа");
-int number1 =int.Parse(Console.ReadLine());
-int number2 =int.Parse(Console.ReadLine());
 
-if(number1>number2)
-{
-    Console.WriteLine("Первое число больше второго");
-}
-else
-{
-    Console.WriteLine("Второе число больше первого");
-}
