49376aae7949506325c15430884cbdb10779af42
Julia <minavninajv@yandex.ru>
2023-06-02 12:00:15 +0300

    add task 19 with function2

--- task19/Program.cs
+++ task19/Program.cs
@@ -3,6 +3,26 @@
 // 12821 -> да
 // 23432 -> да
 
+// Console.WriteLine("Введите пятизначное число: ");
+// int num = int.Parse(Console.ReadLine()!);
+
+// int num1 = num / 10000;
+// int num2 = (num/1000) %10;
+// int num3 = (num/100) %10;
+// int num4 = (num/10) %10;
+// int num5 = num%10;
+
+// Console.Write("{0} {1} {2} {3} {4}" , num1, num2, num3, num4, num5);
+
+// if(num1==num5 && num2==num4)
+// {
+//     Console.WriteLine("Число является палиндромом");
+// }
+// else
+// {
+//     Console.WriteLine("Число не является палиндромом");
+// }
+
 Console.WriteLine("Введите пятизначное число: ");
 int num = int.Parse(Console.ReadLine()!);
 
@@ -12,13 +32,17 @@ int num3 = (num/100) %10;
 int num4 = (num/10) %10;
 int num5 = num%10;
 
-Console.Write("{0} {1} {2} {3} {4}" , num1, num2, num3, num4, num5);
 
-if(num1==num5 && num2==num4)
+void FindPalindrom(int num1, int num2, int num3, int num4, int num5)
 {
-    Console.WriteLine("Число является палиндромом");
+    if(num1==num5 && num2==num4)
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
 }
-else
-{
-    Console.WriteLine("Число не является палиндромом");
-}
+
+FindPalindrom(num1, num2, num3, num4, num5);
