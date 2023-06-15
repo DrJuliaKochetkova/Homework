1d6d46bafa93c7cd02d374649a610aaeef0faba5
Julia <minavninajv@yandex.ru>
2023-06-15 16:39:07 +0300

    vse2

---
+++ task49/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int rows = Prompt("Введите количество строк массива: ");
+int columns = Prompt("Введите количество столбцов массива: ");
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+FindDouble(array);
+Console.WriteLine();
+PrintArray(array);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i+j;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++) // GetLength(0) отвечает за строки
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindDouble(int [,] a)
+{
+        for (int i = 0; i < a.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < a.GetLength(1); j+=2)
+        {
+            a[i, j] = a[i,j]*a[i,j];
+        }
+    }
+}
+
+// for (row = 0 ; row < matr.GetLength(0) ; row ++)
+// {
+// for (col = 0 ; col<matr.GetLength(1) ; col ++)
+// {
+// if (row%2==0 && col%2==0)
+// {
+// temp=matr[row,col]*matr[row,col];
+// matr[row,col]=temp;
+// }
+// }
+// }
---
+++ task50/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// [1,7] -> такого числа в массиве нет [1,7]-это позиция элемента 1 - строка, 7 - столбец
+
+int rows = Prompt("Введите количество строк массива: ");
+int columns = Prompt("Введите количество столбцов массива: ");
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int a = Prompt("Введите параметр символа: ");
+int b = Prompt("Введите параметр символа: ");
+
+FindNumber(array);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++) // GetLength(0) отвечает за строки
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FindNumber(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++) // GetLength(0) отвечает за строки
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (a==i && b==j)
+            {
+                Console.WriteLine("Это позиция элемента: сторока [i], столбец [j]");
+            }
+            else
+            {
+                Console.WriteLine("Такого элемента не существует");
+                break;
+            }
+            
+        }
+        Console.WriteLine();
+    }
+}
+
